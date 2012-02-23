436a0697783a349a439a632b84a1f3a4633dd75b
krobinson <kellyrob@gmail.com>
2012-02-22 22:45:29 -0800

    remove .DS_Store files from index files

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -3,7 +3,7 @@
 for DIR in $(find ./repository -type d); do
   (
     echo -e "<html>\n<body>\n<h1>Directory listing $(date)</h1>\n<hr/>\n<pre>"
-    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
+    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^\.DS_Store$" |  grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
     echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
 done
