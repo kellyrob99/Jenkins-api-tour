54c97754211b79d4c7648db5c2cb1b463a5ddb35
krobinson <kellyrob@gmail.com>
2012-02-25 13:19:41 -0800

    - another sample script

--- scripts/nameAllJobs.groovy
+++ scripts/nameAllJobs.groovy
@@ -2,8 +2,6 @@
 @GrabResolver(name = "github", root = "http://kellyrob99.github.com/Jenkins-api-tour/repository")
 @Grab('org.kar:hudson-api:0.2-SNAPSHOT')
 @GrabExclude('org.codehaus.groovy:groovy')
-import static java.net.HttpURLConnection.*
-import org.kar.hudson.api.*
 import org.kar.hudson.api.cli.HudsonCliApi
 
 String rootUrl = 'http://localhost:8080'
@@ -11,4 +9,5 @@ HudsonCliApi cliApi = new HudsonCliApi()
 OutputStream out = new ByteArrayOutputStream()
 cliApi.runCliCommand(rootUrl, ['groovysh', 'hudson.jobNames.inspect()'], System.in, out, System.err)
 List allJobs = Eval.me(cliApi.parseResponse(out.toString()))
+println allJobs
 
